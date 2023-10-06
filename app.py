26a83d7fcd55c84732de9d1fed7f142f209eb6f4
Dylan-1024040 <1024040@hr.nl>
2023-10-06 18:54:14 +0200

    database aangemaakt

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 
-db = SQLAlchemy()
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqllite://database.db"
-db.init_app(app)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'mykey@12'
 
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False)
+    password = db.column(db.String(80), nullable=False)
 
-@app.route('/home')
+@app.route('/')
 def home():
     return render_template('home.html')
 
